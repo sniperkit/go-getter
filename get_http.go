993af91ba425ac7af3178102004417a26b033f55
Megan Marsh <megan@hashicorp.com>
2017-12-19 12:53:50 -0800

    overhaul getByte Range to make it unexported and to clean up return values and error messages.

--- get_http.go
+++ get_http.go
@@ -4,14 +4,13 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
-
-	"github.com/hashicorp/go-safetemp"
 )
 
 // HttpGetter is a Getter implementation that will download from an HTTP
@@ -122,12 +121,12 @@ func (g *HttpGetter) GetFile(dst string, u *url.URL) error {
 	// check to see whether user has specified a range of bytes to download.
 	// if user has, but the range is invalid, fall back to downloading the
 	// whole file
-	byteRange, rangeErr := GetByteRange(u)
+	byteRange, rangeErr := getByteRange(u)
 	if rangeErr != nil {
-		if strings.Compare("No byte range provided", rangeErr.Error()) != 0 {
-			fmt.Printf("%s; going to download entire file without a range request",
-				rangeErr)
-		}
+		// log that we are downloading whole file even though user
+		// requested a byte range.
+		log.Printf("%s; going to disregard range request and download entire file",
+			rangeErr)
 	}
 
 	req, err := http.NewRequest("HEAD", u.String(), nil)
@@ -265,38 +264,55 @@ func (g *HttpGetter) parseMeta(r io.Reader) (string, error) {
 	}
 }
 
-func GetByteRange(u *url.URL) ([]string, error) {
-	// format can be bytes=startByte- OR bytes=startByte-endByte
-	// example URL string: "http://my/file.iso?ranged_request_bytes=5555-66666"
-	// or  "http://my/file.iso?ranged_request_bytes=5555-"
-	// example bytes_range_retval: []int{5555, 66666} or []int{5555}
-	bytes_range_retval := make([]string, 0)
+func getByteRange(u *url.URL) ([]string, error) {
+	// input values:
+	// u must be non-nil and unmodified.
+	// example of u:
+	// "http://my/file.iso?ranged_request_bytes=5555-66666"
+	// "http://my/file.iso?ranged_request_bytes=5555-"
+	// note that the format of the raw url string can be either
+	// bytes=startByte- OR bytes=startByte-endByte
+	//
+	// return values:
+	// (non-nil, non-nil) - should not occur
+	// (nil, nil) - returned when no byte range was provided in the first place
+	// (nil, non-nil) - error occured when parsing or validating the byte range
+	// (non-nil, nil) - byte range was successfully parsed, and the range is
+	// returned as an array of strings in the following format:
+	// example non-nil return val "vals":
+	// []string{5555, 66666}      // first url example above
+	// []string{5555}             // secomd url example above
+
 	q := u.Query()
 	byteRange := q.Get("ranged_request_bytes")
 	if byteRange == "" {
-		return bytes_range_retval, fmt.Errorf("No byte range provided")
+		return nil, nil
 	}
-	errMsg := fmt.Errorf("Invalid byte range provided")
+
 	vals := strings.Split(byteRange, "-")
 
 	// validate byte range values given
 	if len(vals) != 2 {
-		return bytes_range_retval, errMsg
+		return nil, fmt.Errorf("%s; length of parsed byte range is not 2, %v",
+			invalidRangeMsg, vals)
 	}
 	start, err := strconv.ParseInt(vals[0], 10, 64)
 	if err != nil {
-		return bytes_range_retval, errMsg
+		return nil, fmt.Errorf("%s; could not convert start byte string to an integer",
+			invalidRangeMsg)
 	}
 	// If you leave endBytes blank, read to end of file.
 	if vals[1] != "" {
 		// otherwise make sure "finish" value is a number
 		finish, err := strconv.ParseInt(vals[1], 10, 64)
 		if err != nil {
-			return bytes_range_retval, errMsg
+			return nil, fmt.Errorf("%s; could not convert finish byte string to an integer",
+				invalidRangeMsg)
 		}
 		// need finish to be bigger than start val
 		if finish <= start {
-			return bytes_range_retval, errMsg
+			return nil, fmt.Errorf("%s; finish byte must be bigger than start byte",
+				invalidRangeMsg)
 		}
 	}
 
@@ -328,3 +344,5 @@ func charsetReader(charset string, input io.Reader) (io.Reader, error) {
 		return nil, fmt.Errorf("can't decode XML document using charset %q", charset)
 	}
 }
+
+const invalidRangeMsg = fmt.Errorf("Invalid byte range provided")
